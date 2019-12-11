d49ab034033ea7991e3438a25632c155f31c4c3e
Sarah Ali <asarahali00@gmail.com>
2019-12-11 15:54:08 -0600

    added ingredient list checking function for Paula'sChoice database.

--- checker_input.py
+++ checker_input.py
@@ -1,34 +1,48 @@
 import json
 
 ingredientRequest = input("What ingredient would you like to check? ").upper()
-firstLetter = ingredientRequest[0]
+ingredients = ingredientRequest.split(", ")
 
-if firstLetter.isdigit() or "A" <= firstLetter <= "C":
-    listOrder = ["1","A","B","C"]
-    chosenFile = "pcingr_1toC"
-elif "D" <= firstLetter <= "G":
-    listOrder = ["D","E","F","G"]
-    chosenFile = "pcingr_DtoG"
-elif "H" <= firstLetter <= "L":
-    listOrder = ["H","I","J","K","L"]
-    chosenFile = "pcingr_HtoL"
-elif "M" <= firstLetter <= "O":
-    listOrder = ["M","N","O"]
-    chosenFile = "pcingr_MtoO"
-elif "P" <= firstLetter <= "R":
-    listOrder = ["P","Q","R"]
-    chosenFile = "pcingr_PtoR"
-elif "S" <= firstLetter <= "U":
-    listOrder = ["S","T","U"]
-    chosenFile = "pcingr_StoU"
-elif "V" <= firstLetter <= "Z":
-    listOrder = ["V","W","X","Y","Z"]
-    chosenFile = "pcingr_VtoZ"
+def fileOpener(letter):
+    if letter.isdigit() or "A" <= letter <= "C":
+        listOrder = ["1","A","B","C"]
+        chosenFile = "pcingr_1toC"
+    elif "D" <= letter <= "G":
+        listOrder = ["D","E","F","G"]
+        chosenFile = "pcingr_DtoG"
+    elif "H" <= letter <= "L":
+        listOrder = ["H","I","J","K","L"]
+        chosenFile = "pcingr_HtoL"
+    elif "M" <= letter <= "O":
+        listOrder = ["M","N","O"]
+        chosenFile = "pcingr_MtoO"
+    elif "P" <= letter <= "R":
+        listOrder = ["P","Q","R"]
+        chosenFile = "pcingr_PtoR"
+    elif "S" <= letter <= "U":
+        listOrder = ["S","T","U"]
+        chosenFile = "pcingr_StoU"
+    elif "V" <= letter <= "Z":
+        listOrder = ["V","W","X","Y","Z"]
+        chosenFile = "pcingr_VtoZ"
 
-with open(f"{chosenFile}.json","r") as File:
-    data = json.load(File)
+    return chosenFile, listOrder
 
-    letterSelection = listOrder.index(firstLetter)
-    for ingredientDict in data[letterSelection]:
-        if ingredientDict["ingredient"].upper() == ingredientRequest:
-            print(ingredientDict)
+for ingredient in ingredients:
+    foundFile = fileOpener(ingredient[0])
+    fileName = foundFile[0]
+    fileLetter = foundFile[1].index(ingredient[0])
+    print(f"The ingredient {ingredient}:")
+
+    with open(f"{fileName}.json","r") as File:
+        data = json.load(File)
+
+        for ingredientDict in data[fileLetter]:
+            if ingredientDict["ingredient"].upper() == ingredient:
+                rating = ingredientDict["rating"]
+                desc = ingredientDict["description"]
+                break
+            else:
+                rating = "NOT FOUND"
+                desc = "NOT FOUND"
+        print(f"Rating: {rating}\nDescription: {desc}")
