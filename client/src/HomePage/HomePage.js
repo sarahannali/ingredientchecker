e7dc6375eb4245f48de988e3b52b4b1402111676
Sarah Ali <asarahali00@gmail.com>
2020-03-10 18:57:35 -0500

    added finished dynamic HomePage

--- client/src/HomePage/HomePage.js
+++ client/src/HomePage/HomePage.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import classes from './HomePage.module.css';
-import Header from '../Header/Header'
 import IngrForm from './IngrForm/IngrForm';
 
 function HomePage(props) {
     return (
         <div className={classes.HomePage}>
-            <Header linkhover="#83C6D8"/>
-            <IngrForm />
+            <IngrForm response={props.results}/>
         </div>
     )
 }
