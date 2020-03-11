e3f12fc4233112a7911ea89c342414e0e621c7e4
Sarah Ali <asarahali00@gmail.com>
2020-03-10 21:49:26 -0500

    basic version of results page completed

--- client/src/ResultsPage/ResultsPage.js
+++ client/src/ResultsPage/ResultsPage.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './ResultsPage.module.css';
-import Header from '../Header/Header';
 import ResultsTable from './ResultsTable/ResultsTable'
+import ResultsCards from './ResultsCards/ResultsCards'
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 function ResultsPage(props) {
+    const [value, setValue] = React.useState('cardForm');
+
+    const handleChange = event => {
+        setValue(event.target.value);
+    };
+
     return (
         <div className={classes.ResultsPage}>
-            <Header linkhover="#E88564"/>
             <h3>Verdict:  <span className={classes.purchase}>Unsafe Purchase</span></h3>
-            <ResultsTable />
+            <RadioGroup aria-label="position" name="position" value={value} onChange={handleChange} row>
+                <FormControlLabel
+                    value="cardForm"
+                    control={<Radio color="default" />}
+                    label="Card Form"
+                    labelPlacement="end"
+                />
+                <FormControlLabel
+                    value="tableForm"
+                    control={<Radio color="default" />}
+                    label="Table Form"
+                    labelPlacement="end"
+                />
+            </ RadioGroup>
+            {value === 'cardForm'
+                ? <ResultsCards />
+                : <ResultsTable
+                    sources={props.sources} />}
+            <button className={classes.button}>Start Over</button>
         </div>
     );
 }
