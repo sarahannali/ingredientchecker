043e6afce6219f1de2d3ed0560b2abac6cdcb8c5
Sarah Ali <asarahali00@gmail.com>
2020-03-12 18:23:43 -0500

    fixed ResultsPage to pass down correct props from results.js function

--- client/src/ResultsPage/ResultsPage.js
+++ client/src/ResultsPage/ResultsPage.js
@@ -5,20 +5,40 @@ import ResultsCards from './ResultsCards/ResultsCards'
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
+import results from './Results/Results'
 
 function ResultsPage(props) {
     const [value, setValue] = useState('cardForm');
-    const [purchase, setPurchase] = useState(true)
+    let purchase = true;
+
+    const sourcesToInclude = Object.keys(props.results.sources).filter((el) => {
+        return props.results.sources[el]
+    })
 
     const handleChange = event => {
         setValue(event.target.value);
     };
 
+    const ingredients = props.results.found_names.map((el) => {
+        const result = results(
+            el,
+            sourcesToInclude,
+            props.results.limits.acneLimit,
+            props.results.limits.irrLimit
+        )
+
+        if (result.ingredientType === 'BAD'){
+            purchase = false;
+        }
+
+        return result
+    })
+
     return (
         <div className={classes.ResultsPage}>
             <h3>Verdict:  {purchase
-                ? <span style={{color:'#57A8BE'}} className={classes.purchase}>Safe Purchase</span>
-            : <span style={{color:'#e8916b'}} className={classes.purchase}>Unsafe Purchase</span>}</h3>
+                ? <span style={{ color: '#57A8BE' }} className={classes.purchase}>Safe Purchase</span>
+                : <span style={{ color: '#e8916b' }} className={classes.purchase}>Unsafe Purchase</span>}</h3>
             <RadioGroup className={classes.inputs} aria-label="position" name="position" value={value} onChange={handleChange} row>
                 <FormControlLabel
                     value="cardForm"
@@ -35,17 +55,11 @@ function ResultsPage(props) {
             </ RadioGroup>
             {value === 'cardForm'
                 ? <ResultsCards
-                    sources={props.results.sources}
-                    ingredients={props.results.found_names}
-                    acneLimit={props.results.limits.acneLimit}
-                    irrLimit={props.results.limits.irrLimit}
-                    changePurchase={() => setPurchase(false)}
+                    ingredients={ingredients}
                     />
                 : <ResultsTable
-                    sources={props.results.sources}
-                    ingredients={props.results.found_names}
-                    acneLimit={props.results.limits.acneLimit}
-                    irrLimit={props.results.limits.irrLimit} />}
+                    ingredients={ingredients}
+                    sources={sourcesToInclude} />}
                     
             <a href="/" className={classes.button}>Start Over</a>
         </div>
