89378919043a1843e71f0754a5d55b7233b12d35
Sarah Ali <asarahali00@gmail.com>
2020-03-15 17:17:56 -0500

    update server-side handling of modular scraper files

--- server/scrapers/scrapers.js
+++ server/scrapers/scrapers.js
@@ -4,34 +4,18 @@ const mongodb = require("mongodb");
 async function runScrapers(requestedIngredients) {
     let scraped_ids;
 
-    const cosDNAProcess = spawn('python', ["../scrapers/cosDNA_ingredient_checker.py",
+    const scraperProcess = spawn('python', ["../scrapers/run_scrapers.py",
         requestedIngredients
     ]);
 
-    const INCIProcess = spawn('python', ["../scrapers/inci_ingredient_checker.py",
-        requestedIngredients
-    ]);
-
-    const veganProcess = spawn('python', ["../scrapers/vegan_ingredient_checker.py",
-        requestedIngredients
-    ]);
-
-    cosDNAProcess.stdout.on('data', function (data) {
+    scraperProcess.stdout.on('data', function (data) {
         console.log(data.toString('utf8'));
-    });
-
-    INCIProcess.stdout.on('data', function (data) {
-        console.log(data.toString('utf8'));
-    });
-
-    veganProcess.stdout.on('data', function (data) {
-        data_string = data.toString('utf8');
         scraped_ids = JSON.parse(data_string).ids.map((el) => mongodb.ObjectID(el));
     });
 
-    let cosPromise = new Promise((resolve, reject) => {
-        cosDNAProcess.stdout.on('close', function (code) {
-            console.log(`cosDNA process exited with code ${code}`);
+    let scraperPromise = new Promise((resolve, reject) => {
+        scraperProcess.stdout.on('close', function (code) {
+            console.log(`scraperProcess exited with code ${code}`);
             resolve()
         });
 
@@ -40,29 +24,7 @@ async function runScrapers(requestedIngredients) {
         }, 10000)
     });
 
-    let inciPromise = new Promise((resolve, reject) => {
-        INCIProcess.stdout.on('close', function (code) {
-            console.log(`INCI process exited with code ${code}`);
-            resolve()
-        });
-
-        setTimeout(function () {
-            reject();
-        }, 10000)
-    });
-
-    let veganPromise = new Promise((resolve, reject) => {
-        veganProcess.stdout.on('close', function (code) {
-            console.log(`vegan process exited with code ${code}`);
-            resolve()
-        });
-
-        setTimeout(function () {
-            reject();
-        }, 10000)
-    });
-
-    await Promise.all([cosPromise, inciPromise, veganPromise]);
+    await scraperPromise
 
     return scraped_ids
 }
