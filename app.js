4867f59f3ab9d76f2b156e4515dcd35dc77ccc66
Sarah Ali <asarahali00@gmail.com>
2020-04-08 17:33:13 -0500

    fix HTTPS redirect

--- app.js
+++ app.js
@@ -2,12 +2,21 @@ const bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
     express = require("express"),
     app = express(),
-    path = require("path");
     dotenv = require("dotenv");
 
 if (process.env.ENV == 'development'){
     dotenv.config();
 }
+
+app.enable('trust proxy');
+
+app.use(function (req, res, next) {
+    if (req.secure) {
+            next();
+    } else {
+            res.redirect('https://' + req.headers.host + req.url);
+    }
+});
 
 app.use(express.static("./client/build"));
 app.use(bodyParser.json())
@@ -20,19 +29,6 @@ mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}${p
 });
 
 require('./routes/routes.js')(app);
-app.enable('trust proxy');
-
-app.use (function (req, res, next) {
-    if (req.secure) {
-            next();
-    } else {
-            res.redirect('https://' + req.headers.host + req.url);
-    }
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-})
 
 const port = process.env.PORT || 9000;
 
